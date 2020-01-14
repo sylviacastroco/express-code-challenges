04033339605643aa1fe119075e131f9214126ccc
Sylvia Castro <hello@sylviacastro.co>
2020-01-14 12:29:48 -0600

    Update challenge5.js

--- challenge5.js
+++ challenge5.js
@@ -1,8 +1,12 @@
-/* Write a route to handle PUT requests to /currencies/:name/countries.
+/* 
+Write a route to handle PUT requests to /currencies/:name/countries.
+The :name param represents the currency name in the currencies object.
+The updated array of countries that use this currency will be sent in a query.
 
-The :name param represents the currency name in the currencies object. The updated array of countries that use this currency will be sent in a query.
-
-This route handler should replace the countries array of the correct single-currency object with an updated array from the query. It should send the updated array as a response.*/
+This route handler should replace the countries array of the correct 
+single-currency object with an updated array from the query. 
+It should send the updated array as a response.
+*/
 
 const express = require('express');
 const app = express();
