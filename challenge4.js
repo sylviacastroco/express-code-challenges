8e09399064d78f4afb8684d59824fe9523ec185d
Sylvia Castro <hello@sylviacastro.co>
2020-01-14 12:30:13 -0600

    Update challenge4.js

--- challenge4.js
+++ challenge4.js
@@ -1,4 +1,8 @@
-// Complete the GET /battlefields/:name route. Send back the battlefield object if a battlefield exists, and set the status to 404 and send an empty response if it does not.
+/* 
+Complete the GET /battlefields/:name route. 
+Send back the battlefield object if a battlefield exists, 
+and set the status to 404 and send an empty response if it does not.
+*/
 
 const express = require('express');
 const app = express();
